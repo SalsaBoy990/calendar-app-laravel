7e902b2d8e4998d3938ff063f1e32619b7840c87
Gulácsi András <guland@protonmail.com>
2023-06-19 00:57:45 +0200

    update: Statistics widget
    - Fix job list for recurring events (stats is accurate)
    - TODO: The chart query is not accurate for recurring events (currently, there is no workaround). Regular events are OK.

--- app/Http/Livewire/Statistics/Widget.php
+++ app/Http/Livewire/Statistics/Widget.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire\Statistics;
 
+use DateTime;
+use DateTimeZone;
 use Exception;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
@@ -9,8 +11,7 @@ use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 use Livewire\WithPagination;
 
-class Widget extends Component
-{
+class Widget extends Component {
     use WithPagination;
 
     /**
@@ -35,13 +36,6 @@ class Widget extends Component
      */
     public string $dataOption;
 
-    /**
-     * The month you want to create the statistics
-     *
-     * @var int
-     */
-    public int $month;
-
     public string $startDate;
     public string $endDate;
 
@@ -53,17 +47,19 @@ class Widget extends Component
     public string $chartXAxisTitle;
     public string $chartVAxisTitle;
 
+    public $totalWorks;
+
 
     protected array $rules = [
         //'clientId' => [ 'nullable', 'int', 'max:255' ],
         //'dataOption' => [ 'required', 'string', 'in:all,client' ],
         'startDate' => [ 'nullable', 'date' ],
-        'endDate' => [ 'nullable', 'date' ],
+        'endDate'   => [ 'nullable', 'date' ],
     ];
 
 
     protected $listeners = [
-        'queryData' => 'getStatisticsForChart',
+        'queryData'     => 'getStatisticsForChart',
         'generateChart' => 'generateChart'
     ];
 
@@ -72,66 +68,83 @@ class Widget extends Component
      * @throws Exception
      */
     public function mount() {
-        $this->chartData = null;
+        $this->chartData    = null;
         $this->cleaningJobs = null;
 
-        $this->clientId = 0;
+        $this->clientId   = 0;
         $this->dataOption = 'all';
-        $this->month = 6;
+        $this->totalWorks = null;
 
-        $firstDayOfTheMonth = new \DateTime('first day of this month', new \DateTimeZone('Europe/Budapest'));
-        $lastDayOfTheMonth = new \DateTime('last day of this month', new \DateTimeZone('Europe/Budapest'));
+        $firstDayOfTheMonth = new DateTime( 'first day of this month', new DateTimeZone( 'Europe/Budapest' ) );
+        $lastDayOfTheMonth  = new DateTime( 'last day of this month', new DateTimeZone( 'Europe/Budapest' ) );
 
-        $this->startDate = $firstDayOfTheMonth->format('Y-m-d');
-        $this->endDate = $lastDayOfTheMonth->format('Y-m-d');
+        $this->startDate = $firstDayOfTheMonth->format( 'Y-m-d' );
+        $this->endDate   = $lastDayOfTheMonth->format( 'Y-m-d' );
 
-        $this->chartTitle = 'Hours of cleaning works by clients';
-        $this->chartId = 'chart_div';
-        $this->chartAreaWidth = '65%';
-        $this->chartColor = '#13B623';
+        $this->chartTitle      = 'Hours of cleaning works by clients';
+        $this->chartId         = 'chart_div';
+        $this->chartAreaWidth  = '65%';
+        $this->chartColor      = '#13B623';
         $this->chartXAxisTitle = 'Hours of work';
         $this->chartVAxisTitle = 'Client name';
     }
 
 
-    public function render()
-    {
+    public function render() {
         $this->queryDataForChart();
         $this->getJobList();
 
-        return view('livewire.statistics.widget')->with([
+        return view( 'livewire.statistics.widget' )->with( [
             'cleaningJobs' => $this->cleaningJobs
-        ]);
+        ] );
     }
 
 
-
+    /**
+     * @throws Exception
+     */
     public function getJobList() {
+        $tz        = new DateTimeZone( 'Europe/Budapest' );
+        $startDate = new DateTime( $this->startDate, $tz );
+        $endDate   = new DateTime( $this->endDate, $tz );
+        $interval  = $startDate->diff( $endDate );
+        $weeks = (int) floor($interval->days/7);
+
+
         $result = DB::table( 'events' )
                     ->selectRaw(
                         "clients.name,
                         events.status,
                         events.is_recurring,
                         CASE
-                            WHEN (events.is_recurring = 0) THEN TIME_FORMAT(ABS(TIMEDIFF(events.start, events.end)),'%H:%i')
-                            ELSE TIME_FORMAT(events.duration,'%H:%i')
+                            WHEN (events.is_recurring = 0) THEN
+                                TIME_FORMAT(ABS(TIMEDIFF(events.start, events.end)),'%H:%i')
+                            WHEN (events.is_recurring = 1) THEN
+                                TIME_FORMAT(events.duration,'%H:%i')
                         END AS durationCalc,
 
                         CASE
-                            WHEN (events.is_recurring = 0) THEN TIME_TO_SEC(TIMEDIFF(events.end, events.start)) / 3600
-                            ELSE TIME_TO_SEC(events.duration) / 3600
+                            WHEN (events.is_recurring = 0) THEN
+                                TIME_TO_SEC(TIMEDIFF(events.end, events.start)) / 3600
+                            WHEN (events.is_recurring = 1) THEN
+                                TIME_TO_SEC(events.duration) / 3600 * FLOOR( $weeks / JSON_EXTRACT(`rrule` , '$.interval') )
                         END AS hours,
                         events.start,
-                        events.end"
+                        events.end,
+                        events.rrule"
                     )
                     ->join( 'clients', 'events.client_id', '=', 'clients.id' )
-                    ->whereBetween('events.start', [$this->startDate . ' 00:00:00', $this->endDate . ' 23:59:59'], 'and' )
+
+                    ->whereRaw("events.start > ? AND events.start < ?", [ $this->startDate, $this->endDate ] )
+                    ->orWhereRaw( "DATE(JSON_EXTRACT(events.rrule , '$.dtstart')) > ?", [ $this->startDate ] )
+
                     ->orderByDesc( 'events.end' )
                     ->groupBy( 'clients.name',
                         'events.status',
                         'events.is_recurring',
                         'durationCalc',
                         'hours',
+                        'events.rrule',
                         'events.start',
                         'events.end'
                     )
@@ -141,7 +154,6 @@ class Widget extends Component
     }
 
 
-
     /**
      * @return void
      */
@@ -149,7 +161,7 @@ class Widget extends Component
 
         $this->queryDataForChart();
         $this->getJobList();
-        $this->resetPage('page');
+        $this->resetPage( 'page' );
 
     }
 
@@ -158,17 +170,26 @@ class Widget extends Component
         // validate user input
         $this->validate();
 
+
+        $tz        = new DateTimeZone( 'Europe/Budapest' );
+        $startDate = new DateTime( $this->startDate, $tz );
+        $endDate   = new DateTime( $this->endDate, $tz );
+        $interval  = $startDate->diff( $endDate );
+        $weeks = (int) floor($interval->days/7);
+
         $statistics = DB::table( 'events' )
                         ->selectRaw(
                             "clients.name,
-                CASE
-                    WHEN (events.is_recurring = 0) THEN
-                        SUM( TIME_TO_SEC( TIMEDIFF( events.end, events.start ) ) / 3600 )
-                    ELSE
-                        SUM( TIME_TO_SEC( events.duration ) / 3600 )
-                END AS hours"
-                        )->join( 'clients', 'events.client_id', '=', 'clients.id' )
-                        ->whereBetween('events.start', [$this->startDate . ' 00:00:00', $this->endDate . ' 23:59:59'], 'and' )
+                            SUM(CASE
+                                WHEN (events.is_recurring = 0) THEN
+                                     TIME_TO_SEC( TIMEDIFF( events.end, events.start ) ) / 3600
+                                WHEN (events.is_recurring = 1) THEN
+                                    TIME_TO_SEC(events.duration) / 3600 * FLOOR( $weeks / JSON_EXTRACT(`rrule` , '$.interval') )
+                            END) AS hours"
+                        )
+                        ->join( 'clients', 'events.client_id', '=', 'clients.id' )
+                        ->whereRaw("events.start > ? AND events.start < ?", [ $this->startDate, $this->endDate ] )
+                        ->orWhereRaw( "DATE(JSON_EXTRACT(events.rrule , '$.dtstart')) > ?", [ $this->startDate ] )
                         ->groupBy( 'clients.name', 'events.is_recurring' )
                         ->get();
 
--- resources/views/livewire/statistics/widget.blade.php
+++ resources/views/livewire/statistics/widget.blade.php
@@ -11,6 +11,7 @@
         chartColor: $wire.entangle('chartColor'),
         chartXAxisTitle: $wire.entangle('chartXAxisTitle'),
         chartVAxisTitle: $wire.entangle('chartVAxisTitle'),
+        sumOfHours: 0,
 
         /* Basic chart options */
         getOptions() {
@@ -32,7 +33,9 @@
         /* Creates a 2D array from the PHP array of objects */
         prepareChartData() {
             var dataArray = [];
+            this.sumOfHours = 0;
             for (var i = 0; i < this.chartData.length; i++) {
+                this.sumOfHours += parseFloat(this.chartData[i].hours);
                 dataArray.push([
                     this.chartData[i].name, parseFloat(this.chartData[i].hours)
                 ]);
@@ -55,7 +58,7 @@
             chart.draw(data, this.getOptions());
         }
     }"
-    x-init="
+         x-init="
         // init google chart config with packages to be used
         google.charts.load('current', {packages: ['corechart', 'bar']});
         // Set a callback to run when the Google Visualization API is loaded. laod chart for the first time
@@ -83,27 +86,137 @@
     </div>
 
     <div id="chart_div"></div>
+
+    <h4 class="fs-18">Total number works: <span
+            class="badge gray-60 text-white round">{{ $cleaningJobs->total() }}</span></h4>
+    <h4 class="fs-18">Total working hours: <span class="badge orange-dark text-white round" x-text="sumOfHours"></span>
+    </h4>
+
     <table>
         <thead>
         <tr>
+            <th>#</th>
             <th>Name</th>
             <th>Hours</th>
             <th>Start</th>
             <th>End</th>
-            <th>Status</th>
-            <th>Is recurring?</th>
+            <th>Recurrence</th>
         </tr>
         </thead>
         <tbody>
         @if (isset($cleaningJobs))
+            @php
+                $index = 1;
+				$currentPage = $cleaningJobs->currentPage()
+            @endphp
+
             @foreach($cleaningJobs as $job)
+                @php
+                    $rrule = $job->is_recurring ? json_decode($job->rrule) : null;
+
+					// recurrence '-' or '1 / week'
+					$recurrence = '-';
+					$recurringStartDates = [];
+					$recurringEndDates = [];
+
+					$jobStartDate = $job->start ? substr($job->start, 0, -3) : '';
+					$jobEndDate = $job->end ? substr($job->end, 0, -3): '';
+
+					// For recurring jobs
+					if ($rrule) {
+						$recurrence = $rrule->freq . ' ' . $rrule->interval; // example: '1 / week'
+
+						// variables needed for date calculations between start and end dates of the user-defined-interval
+						$intervalToAdd = '+' . $rrule->interval . ' ' . substr($rrule->freq, 0, -1); // example: +1 week
+					    $tz        = new DateTimeZone( 'Europe/Budapest' );
+						$startDate = new DateTime( $this->startDate, $tz );
+						$endDate = new DateTime( $this->endDate, $tz);
+						$iteratedDate = new DateTime( $rrule->dtstart, $tz);
+						$firstRun = 0;
+
+						while ($iteratedDate <= $endDate) {
+							if ($iteratedDate <= $startDate) {
+								continue;
+							}
+
+							// the first datetime
+							if ($firstRun === 0) {
+							    $recurringStartDates[] = $iteratedDate->format(DateTimeInterface::ATOM);
+
+								// need a new object for the end datetime (not a reference to $iteratedDate)
+								$tempDate = new DateTime($iteratedDate->format(DateTimeInterface::ATOM), $tz);
+
+								// extract duration in minutes
+							    $time = explode(':', $job->durationCalc);
+                                $minutes = ($time[0] * 60.0 + $time[1] * 1.0);
+							    $duration = '+' . $minutes . ' minute';
+
+								// add the duration for the end datetime
+							    $tempDate->modify($duration);
+							    $recurringEndDates[] = $tempDate->format(DateTimeInterface::ATOM);
+
+								$firstRun++;
+								continue;
+							}
+
+							// increase datetime by the recurrence interval (for example: '+1 week')
+							$iteratedDate->modify($intervalToAdd);
+
+							// if the increased datetime is outside the end date of the interval
+							if ($iteratedDate > $endDate) {
+								continue;
+							}
+
+							$recurringStartDates[] = $iteratedDate->format(DateTimeInterface::ATOM);
+
+							// end datetime
+							$tempDate = new DateTime($iteratedDate->format(DateTimeInterface::ATOM), $tz);
+
+							// extract duration in minutes
+							$time = explode(':', $job->durationCalc);
+                            $minutes = ($time[0] * 60.0 + $time[1] * 1.0);
+							$duration = '+' . $minutes . ' minute';
+
+							// add the duration for the end datetime
+							$tempDate->modify($duration);
+							$recurringEndDates[] = $tempDate->format(DateTimeInterface::ATOM);
+						}
+					}
+                @endphp
+
                 <tr>
+                    <td>
+                        @if($currentPage > 1)
+                            {{ ($currentPage - 1) * $cleaningJobs->perPage() + $index++ }}
+                        @else
+                            {{ $index++ }}
+                        @endif
+                    </td>
+
                     <td><b>{{ $job->name }}</b></td>
                     <td>{{ $job->durationCalc }}</td>
-                    <td>{{ $job->start }}</td>
-                    <td>{{ $job->end }}</td>
-                    <td>{{ $job->status }}</td>
-                    <td>{{ $job->is_recurring }}</td>
+
+                    <td>
+                        @if ($jobStartDate !== '')
+                            {{ $jobStartDate }}
+                        @else
+                            @foreach($recurringStartDates as $dateItem)
+                                {{ str_replace('T', ' ', substr($dateItem, 0, -9)) }}<br>
+                            @endforeach
+                        @endif
+                    </td>
+                    <td>
+                        @if ($jobEndDate !== '')
+                            {{ $jobStartDate }}
+                        @else
+                            @foreach($recurringEndDates as $dateItem)
+                                {{ str_replace('T', ' ', substr($dateItem, 0, -9)) }}<br>
+                            @endforeach
+                        @endif
+                    </td>
+
+                    <td>{{ $recurrence  }}</td>
+
                 </tr>
             @endforeach
         @endif
