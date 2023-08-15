0d46d91065ba4e941b54e5e18da82df260d3d02a
Gulácsi András <guland@protonmail.com>
2023-08-15 23:05:31 +0200

    fix: Add check if element exists

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,13 +1,15 @@
 const toggler = document.getElementsByClassName("caret");
-let i;
-
-for (i = 0; i < toggler.length; i++) {
-    toggler[i].addEventListener("click", function () {
-        this.parentElement.parentElement.parentElement.querySelector(".nested").classList.toggle("active");
-        this.classList.toggle("caret-down");
-        this.parentElement.parentElement.classList.toggle("active-list-item");
-    });
+if (toggler) {
+    let i;
+    for (i = 0; i < toggler.length; i++) {
+        toggler[i].addEventListener("click", function () {
+            this.parentElement.parentElement.parentElement.querySelector(".nested").classList.toggle("active");
+            this.classList.toggle("caret-down");
+            this.parentElement.parentElement.classList.toggle("active-list-item");
+        });
+    }
 }
+
 
 // Offcanvas menu
 /* Set the width of the side navigation to 250px */
