c8960e06dd5326e77c126205fa5c6f5490525179
Gulácsi András <guland@protonmail.com>
2023-12-30 17:35:55 +0100

    Remove register endpoints, update header and footer menus

--- resources/views/public/components/footer.blade.php
+++ resources/views/public/components/footer.blade.php
@@ -1,23 +1,14 @@
-<footer class="page-footer public-footer">
+<footer class="page-footer">
     <div class="footer-content">
-        <div class="logo margin-bottom-1">
-            <a href="/" class="brand">
-                <img src="{{ url('/images/logo.png') }}" alt="{{ config('app.name', 'Laravel') }}">
-            </a>
-        </div>
-
-        <nav class="flex flex-row justify-center">
-            <a class="{{ request()->routeIs('blog.index') ? 'active' : '' }}"
-               href="{{ url('/') }}">
-                <i class="fa fa-home margin-right-0-5" aria-hidden="true"></i>{{ __('Home') }}</a>
-
+        <nav>
+            <?php ?>
             @auth
                 <a href="{{ url('/admin/dashboard') }}" class="">{{ __('Dashboard') }}</a>
+            @else
+                <a href="{{ route('login') }}" class="">{{ __('Log in') }}</a>
             @endauth
             <?php ?>
         </nav>
-
-        <small class="text-gray-40 fs-12">&copy; 2023 {{ config('app.name', 'Laravel') }}
-            . {{ __('All rights reserved!') }}</small>
+        <small>&copy; {{ __('2023 SzlaVi Cleaning Team. All rights reserved!') }}</small>
     </div>
 </footer>
--- routes/web.php
+++ routes/web.php
@@ -56,8 +56,8 @@ Route::group(
         Route::get('login', 'App\Http\Controllers\Auth\LoginController@showLoginForm')->name('login');
         Route::post('login', 'App\Http\Controllers\Auth\LoginController@login');
         Route::post('logout', 'App\Http\Controllers\Auth\LoginController@logout')->name('logout');
-        Route::get('register', 'App\Http\Controllers\Auth\RegisterController@showRegistrationForm')->name('register');
-        Route::post('register', 'App\Http\Controllers\Auth\RegisterController@register');
+        // Route::get('register', 'App\Http\Controllers\Auth\RegisterController@showRegistrationForm')->name('register');
+        // Route::post('register', 'App\Http\Controllers\Auth\RegisterController@register');
         /* Login/Logout/Register END */
 
 
